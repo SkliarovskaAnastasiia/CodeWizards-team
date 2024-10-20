a54d3a60f3d2335129c91e242ed08b5dc09a7350
Pavlo Gurskiy <pavlo.gurskiy@gmail.com>
2024-10-20 10:38:41 +0300

    is-active.js delete

--- src/js/is-active.js
+++
@@ -1,7 +0,0 @@
-// document.querySelector('.burger-btn').addEventListener('click', function () {
-//   document.querySelector('.mobile-menu').classList.toggle('is-open');
-// });
-
-document.querySelector('.burger-btn').addEventListener('click', function () {
-  document.querySelector('.mobile-menu').classList.toggle('active');
-});
